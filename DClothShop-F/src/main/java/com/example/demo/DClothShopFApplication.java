6c4ae1d0bf0f0a27aecbe232e0828bf1f6606e3c
MiguelPat0 <84995868+MiguelPat0@users.noreply.github.com>
2022-07-12 18:22:32 -0500

    FIRTS

---
+++ DClothShop-F/src/main/java/com/example/demo/DClothShopFApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DClothShopFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DClothShopFApplication.class, args);
+	}
+
+}
